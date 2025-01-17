24c0f21b1b2c74a5bf427645403e18b73786a9a4
Manu Cortes <manucortespedraja@gmail.com>
2025-01-17 01:03:29 +0100

    add_file

---
+++ Login/Login.cy.js
@@ -0,0 +1,10 @@
+describe('ejercicio login', () => {
+
+    beforeEach(() =>{
+        cy.visit('https://open.spotify.com/intl-es')
+    })
+        it('Loguearse con credenciales correctas',  () => {
+            
+        })
+
+})
